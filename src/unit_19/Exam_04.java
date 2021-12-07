27adc5cd3de3a9453d330b35c9f4d587edb4fd24
DrInnovatio <turing1723@gmail.com>
2021-12-07 18:34:11 +0900

    Toggle Button.

---
+++ src/unit_19/Exam_04.java
@@ -0,0 +1,52 @@
+package unit_19;
+
+import javafx.scene.control.ToggleButton;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+
+class Exam_04_sub extends JFrame{
+
+    private Container con;
+    private GridLayout gl = new GridLayout(2,3,5,5);
+    private JToggleButton tb = new JToggleButton("1", true);
+    private JToggleButton tb1 = new JToggleButton("2", false);
+    private JToggleButton tb2 = new JToggleButton("3", false);
+    private JToggleButton tb3 = new JToggleButton("4", false);
+    private ButtonGroup bg = new ButtonGroup();
+
+    public Exam_04_sub(){
+        super("Poter");
+        this.init();
+        this.start();
+        this.setSize(300, 300);
+        this.setVisible(true);
+    }
+
+    private void init() {
+        con = this.getContentPane();
+        con.setLayout(gl);
+        bg.add(tb);
+        bg.add(tb1);
+        bg.add(tb2);
+        bg.add(tb3);
+
+        con.add(tb);
+        con.add(tb1);
+        con.add(tb2);
+        con.add(tb3);
+
+    }
+
+    private void start() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+public class Exam_04 {
+    public static void main(String[] args) {
+        Exam_04_sub es = new Exam_04_sub();
+    }
+}
