424f3f41d5f88fd79e32e91f0f039b52a457efae
DrInnovatio <turing1723@gmail.com>
2021-12-10 16:43:35 +0900

    How to use Files in Java-1.

---
+++ src/UseOfFiles/Exam_01.java
@@ -0,0 +1,20 @@
+package UseOfFiles;
+
+import java.io.*;
+
+public class Exam_01 {
+
+    public static void main(String[] args) {
+
+        String a = File.pathSeparator;
+        char b = File.pathSeparatorChar;
+        String c = File.separator;
+        char d = File.separatorChar;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+    }
+}
