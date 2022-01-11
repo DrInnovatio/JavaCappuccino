17773dfdce586a738af8604ccd4d2b98c917dea3
DrInnovatio <turing1723@gmail.com>
2022-01-11 09:53:08 +0900

    Max Algorithm.

--- src/Algorithm/MaxAlgorithm.java
+++ src/Algorithm/MaxAlgorithm.java
@@ -1,2 +1,19 @@
-package Algorithm;public class MaxAlgorithm {
+package Algorithm;
+
+public class MaxAlgorithm {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3,7,4,9,5,6,8,1,20,2};
+
+        int currentMax = array[0];
+        for(int i=1;i<=array.length-1;i++){
+            if(currentMax < array[i]){
+                currentMax = array[i];
+
+            }
+        }
+
+        System.out.println("Maximum value is "+ currentMax);
+    }
+
 }
